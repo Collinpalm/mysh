3c6004cc91aec8676c6cf21ae1e7f78c67905ddc
Collin Palm <collinpalm@gmail.com>
2022-02-21 13:35:06 -0500

    seg faults

--- mysh.cpp
+++ mysh.cpp
@@ -5,14 +5,15 @@
 #include <fstream>
 #include <string.h>
 #include <stdlib.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 //function definition
-int history(bool clear);
+int history(bool clear, fstream& historyFile);
 void bye(fstream& historyFile);
 int replay(int number);
-int start(string program, string param);
-int background(string program, string param);
+int start(char* commands[]);
+int background(char* commands[]);
 int terminate(int id);
 void writeToHist(char* commands[]);
 
@@ -24,43 +25,51 @@ int main(void){
 
     string commandLine;
     char* command[4];
-    int i= 0;
-    //get user input
-    cout << "#";
-    getline(cin, commandLine);
-    
-    //copy string format to char* format bc it wasnt working befor
-    char *cstr = new char[commandLine.length() + 1];
-    strcpy(cstr, commandLine.c_str());
-    char *token = strtok(cstr, " ");
-    
-    while(token != NULL){
-        command[i++] = token;
-        token = strtok(NULL, " ");
-    }
-    
-    for(int j = 0;j <= i; j++){
-        historyFile << command[j];
-    }
-    historyFile << endl;
+    while(true){
+        int i= 0;
+        //get user input
+        cout << "\n#";
+        getline(cin, commandLine);
+        
+        //copy string format to char* format bc it wasnt working befor
+        char *cstr = new char[commandLine.length() + 1];
+        strcpy(cstr, commandLine.c_str());
+        char *token = strtok(cstr, " ");
+        
+        while(token != NULL){
+            command[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        //save to history
+        for(int j = 0;j <= i; j++){
+            historyFile << command[j];
+        }
+        historyFile << endl;
 
-    if(strcmp(command[0], "history") == 0){
-        history(command[1]);
-    }else if(strcmp(command[0], "byebye") == 0){
-        bye(historyFile);
-    }else if(strcmp(command[0], "replay") == 0){
-        int j;
-        sscanf(command[1], "%d", &j);
-        replay(j);
-    }else if(strcmp(command[0], "start") == 0){
-        start(command[1], command[2]);
-    }else if(strcmp(command[0], "background") == 0){
-        background(command[1], command[2]);
-    }else if(strcmp(command[0], "terminate") == 0){
-        int j;
-        sscanf(command[1], "%d", &j);
-        terminate(j);
+        //find which command was typed and trigger accompanying function
+        if(strcmp(command[0], "history") == 0){
+            if(command[1] == "-c"){
+                history(true, historyFile);
+            }else{
+                history(false, historyFile);
+            }
+        }else if(strcmp(command[0], "byebye") == 0){
+            bye(historyFile);
+        }else if(strcmp(command[0], "replay") == 0){
+            int j;
+            sscanf(command[1], "%d", &j);
+            replay(j);
+        }else if(strcmp(command[0], "start") == 0){
+            start(command);
+        }else if(strcmp(command[0], "background") == 0){
+            background(command);
+        }else if(strcmp(command[0], "terminate") == 0){
+            int j;
+            sscanf(command[1], "%d", &j);
+            terminate(j);
+        }
     }
+    
 
     bye(historyFile);
     return 0;
@@ -69,7 +78,8 @@ int main(void){
 //function to handle the history command
 //parameter: boolean clear, if true will clear the history
 //returns 1 if success and 0 if fail
-int history(bool clear){
+int history(bool clear, fstream& historyFile){
+    cout << "history";
     return 0;
 }
 
@@ -81,17 +91,21 @@ void bye(fstream& historyFile){
 }
 
 int replay(int number){
+    cout << "replay";
     return 0;
 }
 
-int start(string program, string param){
+int start(char* commands[]){
+    cout << "start";
     return 0;
 }
 
-int background(string program, string param){
+int background(char* commands[]){
+    cout << "background";
     return 0;
 }
 
 int terminate(int id){
+    cout << "terminate";
     return 0;
 }

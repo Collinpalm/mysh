e37401c07e80c72d89ec9617c9261c411acb3ff7
Collin Palm <collinpalm@gmail.com>
2022-02-20 21:02:31 -0500

    tokenizing works, now I need to figure out how to copy it

--- mysh.cpp
+++ mysh.cpp
@@ -1,70 +1,29 @@
 
 #include <unistd.h>     // getpid(), getcwd()
-#include <sys/types.h>  // type definitions, e.g., pid_t
 #include <sys/wait.h>   // wait()
-#include <signal.h>     // signal name constants and kill()
 #include <iostream>
-#include <vector>
-#include <cstring>
+#include <string.h>
 #include <stdlib.h>
 using namespace std;
 
 
-int main()
-{
-  while ( true )
-  {
-    // Show prompt.
-    cout << getcwd(NULL, 0) << ">> " << flush;
-    char command[128];
-    cin.getline( command, 128 );
-
-    vector<char*> args;
-    char* prog = strtok( command, " " );
-    char* tmp = prog;
-    while ( tmp != NULL )
-    {
-      args.push_back( tmp );
-      tmp = strtok( NULL, " " );
+int main(void){
+    string commandLine;
+    char* command[4];
+    int i= 0;
+    //get user input
+    cout << "#";
+    getline(cin, commandLine);
+    
+    //copy string format to char* format bc it wasnt working befor
+    char *cstr = new char[commandLine.length() + 1];
+    strcpy(cstr, commandLine.c_str());
+    char *token = strtok(cstr, " ");
+    
+    while(token != NULL){
+        printf("%s\n", token);
+        token = strtok(NULL, " ");
     }
 
-    char** argv = new char*[args.size()+1];
-    for ( int k = 0; k < args.size(); k++ )
-      argv[k] = args[k];
-
-    argv[args.size()] = NULL;
-
-    if ( strcmp( command, "exit" ) == 0 )
-    {
-      return 0;
-    }
-    else
-    {
-      pid_t kidpid = fork();
-      if ( kidpid < 0 )
-      {
-        perror( "Internal error: cannot fork." );
-        return -1;
-      }
-      else if ( kidpid == 0 )
-      {
-        // I am the child.
-        execvp( prog, argv );
-        // The following lines should not happen (normally).
-        perror( command );
-        return -1;
-      }
-      else
-      {
-        // I am the parent.  Wait for the child.
-        if ( waitpid( kidpid, 0, 0 ) < 0 )
-        {
-          perror( "Internal error: cannot wait for child." );
-          return -1;
-        }
-      }
-    }
-  }
-
-  return 0;
+    return 0;
 }

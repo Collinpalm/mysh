bbc426706beee7b7304083f9962751704626b331
Collin Palm <collinpalm@gmail.com>
2022-02-28 15:21:24 -0500

    I think its all working

--- mysh.cpp
+++ mysh.cpp
@@ -9,15 +9,16 @@
 #include <stdlib.h>
 #include <bits/stdc++.h>
 #include <vector>
+#include <signal.h>
 using namespace std;
 
 //function definition
 int history(vector<string>& hist);
-int replay(vector<string> hist);
+int replay(char*args[], vector<string>& hist);
 int start(char*args[]);
-int background(vector<string> hist);
-int terminate(vector<string> hist);
-void writeToHist(vector<string> hist);
+int background(char*args[]);
+int terminate(char*args[]);
+
 
 int main(void){
     //create history vector
@@ -26,7 +27,7 @@ int main(void){
     int i, success;
     bool loopcheck = true;
     string commandLine;
-    char* command[4];
+    char* command[10];
     cout << "#";
     while(loopcheck){
         i= 0;
@@ -56,18 +57,18 @@ int main(void){
             success = history(histv);
         }else if(strcmp(command[0], "byebye") == 0){
             loopcheck = false;
-            success = 1;
+            success = 0;
             break;
         }else if(strcmp(command[0], "replay") == 0){
-            success = replay(histv);
+            success = replay(command, histv);
         }else if(strcmp(command[0], "start") == 0){
             success = start(command);
         }else if(strcmp(command[0], "background") == 0){
-            success = background(histv);
+            success = background(command);
         }else if(strcmp(command[0], "terminate") == 0){
-            success = terminate(histv);
+            success = terminate(command);
         }
-        if(success != 1){
+        if(success != 0){
             cout << "Your command failed, try running this shell with sudo";
         }
         cout << "#";
@@ -86,22 +87,60 @@ int history(vector<string>& hist){
     //variables
     if(hist.back().find("-c") != string::npos){
         hist.clear();
-        return 1;
+        return 0;
     }
     for (int j = hist.size()-1; j>=0;j--){
         cout << hist.size()-(j+1) << ": " << hist[j] << endl;
     }
 
-    return 1;
+    return 0;
 }
 
 
-int replay(vector<string> hist){
-    cout << "replay"<< endl;
-    return 1;
+int replay(char*args[], vector<string>& hist){
+    int pos = (hist.size()-1)-(atoi(args[1])+1);
+    int i = 0;
+    char *cstrs = new char[hist[pos].length() + 1];
+    strcpy(cstrs, hist[pos].c_str());
+    char *tokens = strtok(cstrs, " ");
+    char* rcommand[10];
+
+    while(tokens != NULL){
+        rcommand[i++] = tokens;
+        tokens = strtok(NULL, " ");
+    }
+    
+    if(strcmp(rcommand[0], "history") == 0){
+        history(hist);
+    }else if(strcmp(rcommand[0], "start") == 0){
+        start(rcommand);
+    }else if(strcmp(rcommand[0], "background") == 0){
+        background(rcommand);
+    }else if(strcmp(rcommand[0], "terminate") == 0){
+        terminate(rcommand);
+    }
+    return 0;
 }
 
 int start(char*args[]){
+    pid_t pid;
+    pid = fork();
+
+    if(pid < 0){
+        cout << stderr<< "fork fail"<< endl;
+        return 1;
+    }else if(pid == 0){
+        execvp(args[1], args);
+    }else {
+        pid = wait(NULL);
+    }
+    
+
+
+    return 0;
+}
+
+int background(char*args[]){
     pid_t cpid;
     cpid = fork();
 
@@ -109,22 +148,17 @@ int start(char*args[]){
         cout << stderr<< "fork fail"<< endl;
         return 1;
     }else if(cpid == 0){
-        execvp(args[1], args);
-    }else {
-        cpid = wait(NULL);
+        cpid = execvp(args[1], args);
     }
+    cout<<cpid<<endl;
     
-
-
-    return 1;
+    return 0;
 }
 
-int background(vector<string> hist){
-    cout << "background"<< endl;
-    return 1;
-}
-
-int terminate(vector<string> hist){
-    cout << "terminate"<< endl;
-    return 1;
+int terminate(char*args[]){
+    int val = 1;
+    if(0==kill(atoi(args[1]), 0)){
+        val = kill(atoi(args[1]), SIGKILL);
+    }
+    return val;
 }
